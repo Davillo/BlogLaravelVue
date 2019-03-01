5dad67cb2305d5f36266732924ce0c77a198c833
Davillo <=>
2019-03-01 11:10:35 -0300

    novos componentes vue e configurações de front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <example-component></example-component>
+<div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <painel titulo="Dashboard">
+                Teste de conteúdo 
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <caixa></caixa>
+                    </div>
+
+                    <div class="col-md-4">
+                        <painel titulo="Conteudo 2" cor="panel-warning">
+                            teste conteudo 2
+                        </painel>
+                    </div>
+
+                    <div class="col-md-4">
+                        <painel titulo="Conteudo 3" cor="panel-danger">
+                            teste conteudo 3
+                        </painel>
+                    </div>
+                </div>
+
+                </painel>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,32 +15,9 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
+        <topo titulo="{{ config('app.name', 'Laravel') }}" url="{{ url('/') }}">
+               <!-- Right Side Of Navbar -->
+               <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
@@ -67,9 +44,12 @@
                             </li>
                         @endguest
                     </ul>
-                </div>
-            </div>
-        </nav>
+        </topo>
+
+      
+
+             
+              
 
         @yield('content')
     </div>
